8c117f8f83beef07bea36a74140c39b65ac508e8
SupStrong <1458380717@qq.com>
2020-12-21 21:38:04 +0800

    main

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devServer: {
       proxy: {
         '/api': {
-            target: 'http://101.200.61.236:7001',//后端接口地址
+            target: 'http://localhost:7001',//后端接口地址
             changeOrigin: true,//是否允许跨越
             pathRewrite: {
               '^/api/': '/' 
